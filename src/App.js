21a9d7d047f8c29b194bf73938649d3dff16c541
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-04-05 08:44:24 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Final from './tutorial/1-useState/final/3-useState-array';
-
-
+import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data';
 
 function App() {
 	return (
 		<div className="container">
-			<Final />
-		
+			<Setup />
 		</div>
 	);
 }
--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,22 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+	const [value, setValue] = useState(0);
+	useEffect(() => {
+		console.log('call UseEffect');
+		if (value >= 1) {
+			document.title = `New Messages(${value})`;
+		}
+	},[value]);
+	console.log('render component');
+	return (
+		<>
+			<h1>{value}</h1>
+			<button className="btn" onClick={() => setValue(value + 1)}>
+				CLick
+			</button>
+		</>
+	);
 };
 
 export default UseEffectBasics;
